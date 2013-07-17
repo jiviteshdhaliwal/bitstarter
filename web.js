f1b8cf6e8d4dbeee1cdfdb6748dfa492d291770d
= <=>
2013-07-17 16:48:30 +0530

    Added hardcoded text

--- web.js
+++ web.js
@@ -4,8 +4,8 @@ var app = express.createServer(express.logger());
 
 var stringBuf = fs.readFileSync('/home/jivitesh/startup/bitstarter/index.html');
 
-app.get('/', function(request, response, stringBuf) {
-  response.send(stringBuf.toString());
+app.get('/', function(request, response) {
+  response.send("HELLO WORLD");
 });
 
 var port = process.env.PORT || 5000;
