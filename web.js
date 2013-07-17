abf1959357af6b3cf8bf7825ea0aff128290cfe1
= <=>
2013-07-17 16:21:22 +0530

    corrected syntactical error

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var stringBuf = fs.readFileSync('/home/jivitesh/startup/index.html')
+var stringBuf = fs.readFileSync('/home/jivitesh/startup/index.html');
 
 app.get('/', function(request, response) {
   response.send(stringBuf.toString());
