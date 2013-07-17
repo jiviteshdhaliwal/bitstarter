0415ff785c5496078ae3d656a3ec8990cd2e54c7
= <=>
2013-07-17 16:03:21 +0530

    corrected web js to perform read operation from file

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var stringBuf = fs.readFileSync('/home/jivitesh/startup/index.html')
+
 app.get('/', function(request, response) {
-  response.send('Hello! This is your creator Jivitesh!');
+  response.send(stringBuf.toString());
 });
 
 var port = process.env.PORT || 5000;
